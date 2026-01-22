03c72798cc73b1bb70b118ec075e0d78eb314a18
sandeep <sandeep@sandeeps-MacBook-Air.local>
2026-01-22 22:17:32 +0545

    Fix: Allow media serving when DEBUG=False for VPS preview

--- core/urls.py
+++ core/urls.py
@@ -35,7 +35,12 @@ handler500 = 'apps.adminapp.views.error_500'
 
 # Add static and media file serving
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.views.static import serve
+from django.urls import re_path
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+]
 
 
 
