6223cf540098416746f9a89375a7c7fd011995e9
sandeep <sandeep@sandeeps-MacBook-Air.local>
2026-01-23 18:15:13 +0545

    fix: resolve DisallowedHost by cleaning ALLOWED_HOSTS parsing and updating production config

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-x=qe5@^3%@t1fk)pk@uyv&r!z^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [host.strip("\"' ") for host in os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')]
 
 
 # Application definition
